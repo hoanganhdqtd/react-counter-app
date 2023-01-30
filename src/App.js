7af5b7c35e39e00ae111fe58bf7fc71b45c10a9a
Anh Phan <anhphan@Anhs-MacBook-Pro.local>
2023-01-30 22:08:07 +0700

    Add counter app by React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  let color = counter > 0 ? "green" : counter < 0 ? "red" : "black";
+
+  const increase = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrease = () => {
+    setCounter(counter - 1);
+  };
+
+  const reset = () => {
+    setCounter(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1>Counter</h1>
+      <span id="value" style={{ color }}>
+        {counter}
+      </span>
+      <div class="btn-group">
+        <button class="btn btn-decrease" onClick={decrease}>
+          Decrease
+        </button>
+        <button class="btn btn-reset" onClick={reset}>
+          Reset
+        </button>
+        <button class="btn btn-increase" onClick={increase}>
+          Increase
+        </button>
+      </div>
     </div>
   );
 }
